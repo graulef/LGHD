25711c472ee1338975a173dfc917c1cd17fd5a1b
graulef <felix@graule.ch>
2019-04-08 15:34:37 +0200

    Move writing keypoint image to verbose block

--- src/examples/lghd.cpp
+++ src/examples/lghd.cpp
@@ -49,11 +49,13 @@ std::vector<cv::KeyPoint> get_keypoints(cv::Mat image, std::string spectrum) {
     std::cout << "Building descriptors for the " << keypoints.size() << " strongest keypoints." << std::endl;
 
     // Save image with kept keypoints to disk
-    cv::Mat draw;
-    cv::drawKeypoints(image, keypoints, draw);
-    char keypoint_filename[32];
-    sprintf(keypoint_filename, "%s_keypoints.jpg", spectrum.c_str());
-    cv::imwrite(keypoint_filename, draw);
+    if (VERBOSE) {
+        cv::Mat draw;
+        cv::drawKeypoints(image, keypoints, draw);
+        char keypoint_filename[32];
+        sprintf(keypoint_filename, "%s_keypoints.jpg", spectrum.c_str());
+        cv::imwrite(keypoint_filename, draw);
+    }
 
     return keypoints;
 }
@@ -273,14 +275,14 @@ cv::Mat generate_lgdh_descriptor(cv::Mat image, std::vector<cv::KeyPoint> keypoi
 int main(int argc, char *argv[])
 {
     // Load RGB image & detect keypoints
-    const cv::Mat rgb_image = cv::imread("data/20160719_200545.jpg", cv::IMREAD_GRAYSCALE);
+    const cv::Mat rgb_image = cv::imread("data/20160719_191350.jpg", cv::IMREAD_GRAYSCALE);
     std::vector<cv::KeyPoint> rgb_kps = get_keypoints(rgb_image, "rgb");
 
     // Generate LGHD descriptor for RGB
     cv::Mat rgb_descr = generate_lgdh_descriptor(rgb_image, rgb_kps, "rgb");
 
     // Load infrared image & detect keypoints
-    const cv::Mat ir_image = cv::imread("data/20160719_200542.jpg", cv::IMREAD_GRAYSCALE);
+    const cv::Mat ir_image = cv::imread("data/20160719_191349.jpg", cv::IMREAD_GRAYSCALE);
     std::vector<cv::KeyPoint> ir_kps = get_keypoints(ir_image, "ir");
 
     // Generate LGHD descriptor for infrared
